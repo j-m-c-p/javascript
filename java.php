12a7fda289f2540b3e1a5bdbb0e1c84c1f48533a
j-m-c-p <jhonatancubi@gmail.com>
2016-09-05 12:34:45 -0500

    Add files via upload
    Este es el archivo php con lineas de código script que permite repetir de 1 a 251 veces la imagen con la enumeración

---
+++ java.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<script>
+
+		for(var i=1;i<201;i++ ){ 
+		document.write(i + " ");
+		document.write(" <strong>H</strong>ola <br><img src='imagenes/Cj.png'><br>"); 
+		} 
+
+	</script>
+
+</body>
+</html>
